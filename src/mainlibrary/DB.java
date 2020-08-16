6bc35bbf594c01d7010cf49a1cbc4abd128bf735
ValMan <x19186681@student.ncirl.ie>
2020-08-16 14:47:09 +0100

    Repo init

---
+++ src/mainlibrary/DB.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainlibrary;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author bikash
+ */
+public class DB {
+
+    public static String user = "root";
+    public static String connection = "jdbc:mysql://172.17.0.1:3306/library";
+    
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Properties props = new Properties();
+            props.put("user", user);
+            props.put("password", "Chivalrous999");
+            props.put("useUnicode", "true");
+            props.put("useSSL", "false");
+            props.put("useServerPrepStmts", "false"); // use client-side prepared statement
+            props.put("characterEncoding", "UTF-8"); // ensure charset is utf8 here
+
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(connection, props);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return con;
+    }
+
+}
