3e164b1cdf43335392b62b5482fb7cf3b77290d5
ValMan <x19186681@student.ncirl.ie>
2020-08-21 19:18:08 +0100

    Config added

--- src/mainlibrary/DB.java
+++ src/mainlibrary/DB.java
@@ -16,17 +16,17 @@ import java.util.Properties;
  */
 public class DB {
 
-    public static String user = "root";
-    public static String connection = "jdbc:mysql://172.17.0.1:3306/library";
-    
+
     public static Connection getConnection() {
         Connection con = null;
         try {
+            Config cfg = new Config();
             Properties props = new Properties();
-            props.put("user", user);
-            props.put("password", "Chivalrous999");
-            props.put("useUnicode", "true");
-            props.put("useSSL", "false");
+            String connection = cfg.getProperty("ConnStr"); //"jdbc:mysql://172.17.0.1:3306/library";
+            props.put("user", cfg.getProperty("UserName"));
+            props.put("password", cfg.getProperty("Password"););
+            props.put("useUnicode", cfg.getProperty("useUnicode"););
+            props.put("useSSL", cfg.getProperty("useSSL"););
             props.put("useServerPrepStmts", "false"); // use client-side prepared statement
             props.put("characterEncoding", "UTF-8"); // ensure charset is utf8 here
 
